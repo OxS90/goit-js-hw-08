c97d6f6099abb0b8154dc461480891566dd21dd6
OxS90 <oxana.slivinschi@gmail.com>
2023-11-07 06:44:30 +0200

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const { SimpleLightbox } = require('simplelightbox/dist/simple-lightbox.js');
+
+// Import suplimentar de stil
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+function createGalleryItem(item) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+galleryItems.forEach(item => {
+  const galleryItem = createGalleryItem(item);
+  galleryList.appendChild(galleryItem);
+});
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
